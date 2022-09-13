7acaa02f7566f64cdaab1a4605dc231c1820f553
georgkiim <kimkim99975@gmail.com>
2022-09-13 19:30:36 +0300

    Update 10.js

--- 10.js
+++ 10.js
@@ -1,1 +1,10 @@
 const index = (array, n) => array.length > n ? Math.pow(array[n], n) : -1;
+
+function billboard(name, price = 30){
+    const nameArray = name.split("");
+    return multiply(nameArray.length,price)
+      
+    } 
+    const multiply = (num1, num2) => {
+      return num1 * num2;
+    };
